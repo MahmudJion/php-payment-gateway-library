afda69cf0a6b00ac57630256c4b86637644621ff
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2023-03-07 12:34:59 +0000

    update bkash.php

--- bKash/bkash.php
+++ bKash/bkash.php
@@ -8,10 +8,13 @@ $url = "https://www.bkashcluster.com:9081"; // bKash API URL with Port Number
 $trxid = "66666AAAAA"; // bKash Transaction ID : TrxID
 
 // Final URL for getting response from bKash
-$bkash_url = $url.'/dreamwave/merchant/trxcheck/sendmsg?user='.$user.'&pass='.$pass.'&msisdn='.$msisdn.'&trxid='.$trxid;
+$bkash_url = "{$url}/dreamwave/merchant/trxcheck/sendmsg?user={$user}&pass={$pass}&msisdn={$msisdn}&trxid={$trxid}";
 
+// Initiate a cURL session
 $curl = curl_init();
-curl_setopt_array($curl, array(
+
+// Set cURL options
+curl_setopt_array($curl, [
     CURLOPT_PORT => 9081,
     CURLOPT_URL => $bkash_url,
     CURLOPT_RETURNTRANSFER => true,
@@ -20,32 +23,43 @@ curl_setopt_array($curl, array(
     CURLOPT_TIMEOUT => 30,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => "GET",
-    CURLOPT_HTTPHEADER => array(
+    CURLOPT_HTTPHEADER => [
         "cache-control: no-cache",
-        "content-type: application/json"
-    ),
-));
+        "content-type: application/json",
+    ],
+]);
 
+// Send the cURL request and capture the response
 $response = curl_exec($curl);
-$err = curl_error($curl);
 
+// Check for errors
+if ($error = curl_error($curl)) {
+    echo "Problem for Sending Response to bKash API! Try Again after few minutes. Error: {$error}";
+    exit;
+}
+
+// Get the HTTP response code
 $http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+// Close the cURL session
 curl_close($curl);
 
-$api_response = json_decode ($response, true);  // Getting Response from bKash API.
-$transaction_status = $api_response['transaction']['trxStatus']; // Transaction Status Codes
+// Decode the JSON response from bKash
+$api_response = json_decode($response, true);
 
-    if ($err || $transaction_status == "4001") {
-        echo 'Problem for Sending Response to bKash API ! Try Again after fews minutes.';
-    }
-    else
-    {
-        // Assign Transaction Information
-        $transaction_amount = $api_response['transaction']['amount']; // bKash Payment Amount
-        $transaction_reference = $api_response['transaction']['reference']; // bKash Reference for Invoice ID
-        $transaction_time = $api_response['transaction']['trxTimestamp']; // bKash Transaction Time & Date
+// Get the transaction status code
+$transaction_status = $api_response['transaction']['trxStatus'];
 
-        // Print Transaction Information
-        echo $transaction_status."<br>".$transaction_amount."<br>".$transaction_reference."<br>".$transaction_time;
-    }
-?>
+// Check if there was an error or if the transaction was successful
+if ($http_code !== 200 || $transaction_status !== "0000") {
+    echo "Problem with the bKash transaction! Status: {$transaction_status}";
+    exit;
+}
+
+// Assign Transaction Information
+$transaction_amount = $api_response['transaction']['amount']; // bKash Payment Amount
+$transaction_reference = $api_response['transaction']['reference']; // bKash Reference for Invoice ID
+$transaction_time = $api_response['transaction']['trxTimestamp']; // bKash Transaction Time & Date
+
+// Print Transaction Information
+echo "{$transaction_status}<br>{$transaction_amount}<br>{$transaction_reference}<br>{$transaction_time}";
